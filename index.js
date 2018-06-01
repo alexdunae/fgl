8766571ce5eb351fd45704352fd5a74017ecd2d4
Alex Dunae <alex@dunae.ca>
2018-06-01 12:34:54 -0700

    Further implementation

--- index.js
+++ index.js
@@ -1,3 +1,30 @@
+const ESC = '\x1B';
+
+/**
+ * @constant
+ *
+ * @type {string[]}
+ */
+export const IMAGE_TYPES = ['fgl', 'pcx', 'bmp'];
+
+function checkRange(name, value, min, max) {
+  if (value < min || value > max) throw new Error(`${name} must be between ${min} and ${max}`);
+}
+
+function checkType(name, value, types) {
+  if (!types.includes(value)) throw new Error(`${name} must be one of ${types.join(', ')}`);
+}
+
+function formatCmd(format) {
+  return format == 'fgl' ? '' : cmd(format);
+}
+
+/**
+ * Print a generic FGL command
+ * @param {string} string The command string, excluding the brackets
+ * @param {string} [text] Text to encode immediately after the command
+ * @returns {string}
+ */
 export function cmd(string, text) {
   if (text) {
     return `<${string}>^${text}^`;
@@ -6,78 +33,11 @@ export function cmd(string, text) {
   }
 }
 
-export function move(row, col) {
-  return cmd(`RC${row},${col}`);
-}
-
-export function boxSize(w, h) {
-  return cmd(`BS${w},${h}`);
-}
-
-export function logoStartingPoint(y, x) {
-  return cmd(`SP${y},${x}`);
-}
-
-export function printLogo(id) {
-  return cmd(`LD${id}`);
-}
-
-export function code128Ladder(data, height = 10) {
-  return cmd(`OL${height}`, data);
-}
-
-export function code128PicketFence(data, height = 10) {
-  return cmd(`OP${height}`, data);
-}
-
-// const QR_VERSIONS = [2, 7, 11, 15];
-
-export function qrCode(data, size = 3, applyTilde = false, encodeMode = 0, errorCorrection = 0) {
-  if (size < 3 || size > 16) throw new Error('size must be between 3 and 16');
-  if (encodeMode < 0 || encodeMode > 2) throw new Error('encodeMode must be between 0 and 2');
-  if (errorCorrection < 0 || errorCorrection > 3)
-    throw new Error('errorCorrection must be between 3 and 16');
-
-  return cmd(`QR${size},${applyTilde ? 0 : 1},${errorCorrection},`, data);
-}
-
-export function drawBox(w, h) {
-  return cmd(`BX${w},${h}`);
-}
-
-export function fontSize(size = 3) {
-  return cmd(`F${size}`);
-}
-
-export function barcodeInterpretation() {
-  return cmd('BI');
-}
-
-export function printCut() {
-  return cmd('p');
-}
-
-export function printCutHold() {
-  return cmd('h');
-}
-
-export function printNoCut() {
-  return cmd('q');
-}
-
-export function printNoCutHold() {
-  return cmd('r');
-}
-
-export function ticketCount() {
-  return cmd('PC');
-}
-
-export function setTicketCount(count) {
-  // left-pad!!
-  return cmd(`TC${count.toString().padStart(7, '0')}`);
-}
-
+/**
+ * Set the rotation
+ * @param {number} degrees May be 0, 90, 180 or 270
+ * @returns {string}
+ */
 export function rotate(deg) {
   let string = 'NR';
   switch (deg) {
@@ -96,16 +56,310 @@ export function rotate(deg) {
   return cmd(string);
 }
 
+/**
+ * @param {number} row
+ * @param {number} col
+ * @returns {string}
+ */
+export function move(row, col) {
+  return cmd(`RC${row},${col}`);
+}
+
+/**
+ * @param {number} w
+ * @param {number} h
+ * @returns {string}
+ */
+export function boxSize(w, h) {
+  return cmd(`BS${w},${h}`);
+}
+
+function fontHeightWidth(h, w) {
+  return cmd(`HW${h},${w}`);
+}
+
+// <F1>  Font1 characters (5x7)
+// <F2>  Font2 characters (8x16)
+// <F3>  OCRB (17x31)
+// <F4>  OCRA (5x9)
+// <F6>  large OCRB (30x52)
+// <F7>  OCRA (15x29)
+// <F8>  Courier (20x40)(20x33)
+// <F9>  small OCRB (13x20)
+// <F10> Prestige (25x41)
+// <F11> Script (25x49)
+// <F12> Orator (46x91)
+// <F13> Courier (20x40)(20X42)
+
+/**
+ * Use an internal font, optionally setting the height and width
+ * @param {number} id Font ID
+ * @param {number} [h] Height
+ * @param {number} [w] Width
+ * @returns {string}
+ */
+export function useFont(id, h, w) {
+  const sizeCmd = h ? fontHeightWidth(h, w) : '';
+  return sizeCmd + cmd(`F${id}`);
+}
+
+/**
+ * @param {number} id ID for identifying the font
+ * @param {string} bytes Hex-encoded bytes
+ * @returns {string}
+ */
+export function uploadTTF(id, bytes) {
+  checkRange('id', id, 0, 511);
+
+  return cmd(`ID${id}`) + cmd(`ttf${bytes.length}`) + bytes;
+}
+
+/**
+ * Start using an uploaded TTF font
+ * @param {number} id Font ID
+ * @param {number} size Font size
+ * @returns {string}
+ */
+export function useTTF(id, size) {
+  return cmd(`TTF${id},${size}`);
+}
+
+/**
+ * @param {number} id ID for identifying the logo
+ * @param {string} bytes Hex-encoded bytes
+ * @param {string} [format=fgl]
+ * @returns {string}
+ */
+export function uploadImageLogo(id, bytes, format = 'fgl') {
+  checkRange('id', id, 0, 511);
+  checkType('format', format, IMAGE_TYPES);
+
+  return (
+    ESC +
+    cmd(`ID${id}`) +
+    move(0, 0) +
+    formatCmd(format) +
+    cmd(`G${bytes.length}`) +
+    bytes +
+    move(0, 0) +
+    ESC
+  );
+}
+
+/**
+ * Create a text-based logo
+ * @param {string[]} cmds An array of commands that make up the text logo
+ * @todo does this accept an ID?
+ * @returns {string}
+ */
+export function uploadTextLogo(cmds) {
+  return ESC + Array.prototype.join.call(arguments, '') + ESC;
+}
+
+/**
+ * @param {number} id
+ * @param {number} row
+ * @param {number} col
+ * @param {string} [format='fgl']
+ * @returns {string}
+ */
+export function printLogo(id, row, col, format = 'fgl') {
+  checkType('format', format, IMAGE_TYPES);
+  return cmd(`SP${row},${col}`) + formatCmd(format) + cmd(`LD${id}`);
+}
+
+// const QR_VERSIONS = [2, 7, 11, 15];
+
+/**
+ * @param {string} data The data to encode
+ * @param {number} [size=3]
+ * @param {boolean} [applyTilde=false]
+ * @param {number} [encodeMode=0] 0-2
+ * @param {number} [errorCorrection=0] 0-3
+ * @returns {string}
+ */
+export function qrCode(data, size = 3, applyTilde = false, encodeMode = 0, errorCorrection = 0) {
+  //export function qrCode(data, size = 3, applyTilde = false, encodeMode = 0, errorCorrection = 0) {
+  checkRange('size', size, 3, 16);
+  checkRange('encodeMode', encodeMode, 0, 2);
+  checkRange('errorCorrection', errorCorrection, 0, 3);
+
+  return cmd(`QR${size},${applyTilde ? 0 : 1},${errorCorrection},`, data);
+}
+
+/**
+ * @param {string} data The data to encode
+ * @param {boolean} [ladder=true] Ladder or picket-fence orientation
+ * @param {number} [height=10]
+ * @returns {string}
+ */
+export function code128(data, ladder = true, height = 10) {
+  const orientation = ladder ? 'L' : 'P';
+  return cmd(`o${orientation}${height}`, data);
+}
+
+
+/**
+ * @param {string} data The data to encode
+ * @param {number} [size=3]
+ * @param {boolean} [applyTilde=false]
+ * @param {number} [encodeMode=0] 0-2
+ * @param {number} [errorCorrection=0] 0-3
+ * @returns {string}
+ */
+export function barcodeInterpretation() {
+  return cmd('BI');
+}
+
+// 0 - white
+// 1 - very light course dot pattern
+// 2 - light course dot pattern
+// 3 - light - medium course dot pattern
+// 4 - medium course dot pattern
+// 5 - dark - medium course dot pattern
+// 6 - dark course dot pattern
+// 7 - open
+// 8 - open
+// 9 - black
+// 10 - white
+// 11 - very light fine dot pattern
+// 12 - light fine dot pattern
+// 13 - light - medium fine dot pattern
+// 14 - medium fine dot pattern
+// 15 - dark - medium fine dot pattern
+// 16 - dark fine dot pattern
+// 17 - open
+// 18 - open
+// 19 - black
+// 20 - vertical lines
+// 21 - horizontal lines
+// 22 - forward diagonal lines
+// 23 - backward diagonal lines
+// 24 - square grid
+// 25 - diagonal grid
+
+/**
+ * Set the shading pattern
+ * @param {string} data
+ * @param {number} [id=14]
+ * @param {boolean} [background=true]
+ * @returns {string}
+ */
+export function shadingPattern(data, id = 14, background = true) {
+  checkRange('id', id, 0, 25);
+  return cmd(`PA${background ? 'B' : 'F'}`) + cmd(`PA${id}`) + cmd('ES') + data, cmd('DS');
+}
+
+function lineThicknessCmd(lineThickness) {
+  return lineThickness ? cmd(`LT${lineThickness}`) : '';
+}
+
+/**
+ * @param {number} w
+ * @param {number} h
+ * @param {number} lineThickness
+ * @returns {string}
+ */
+export function drawBox(w, h, lineThickness) {
+  return lineThicknessCmd(lineThickness) + cmd(`BX${w},${h}`);
+}
+
+/**
+ * @param {number} size
+ * @param {number} lineThickness
+ * @returns {string}
+ */
+export function horizontalLine(size, lineThickness) {
+  return lineThicknessCmd(lineThickness) + cmd(`HX${size}`);
+}
+
+/**
+ * @param {number} size
+ * @param {number} lineThickness
+ * @returns {string}
+ */
+export function verticalLine(size, lineThickness) {
+  return lineThicknessCmd(lineThickness) + cmd(`VX${size}`);
+}
+
+/**
+ * Print the ticket
+ * @param {boolean} cut Print and cut or just print?
+ * @returns {string}
+ */
+export function print(cut = false) {
+  return cmd(cut ? 'q' : 'p');
+}
+
+/**
+ * Print the ticket and hold
+ * @param {boolean} cut Print and cut or just print?
+ * @returns {string}
+ */
+export function printHold(cut) {
+  return cmd(cut ? 'h' : 'r');
+}
+
+/**
+ * Print the current ticket count
+ * @returns {string}
+ */
+export function ticketCount() {
+  return cmd('PC');
+}
+
+/**
+ * Set the number of tickets to print
+ * @param {count}
+ * @returns {string}
+ */
+export function setTicketCount(count) {
+  // left-pad!!
+  return cmd(`TC${count.toString().padStart(7, '0')}`);
+}
+
+/**
+ * Print alignCenter text
+ * @param {string} text
+ * @param {number} fieldWidth
+ * @returns {string}
+ */
+export function alignCenter(text, fieldWidth) {
+  return cmd(`CTR${fieldWidth}`) + `~${text}~`;
+}
+
+/**
+ * Print right-justified text
+ * @param {string} text
+ * @param {number} fieldWidth
+ * @returns {string}
+ */
+export function alignRight(text, fieldWidth) {
+  return cmd(`RTJ${fieldWidth}`) + `~${text}~`;
+}
+
 export function run() {
   const cmds = [
+    move(0, 0),
+    shadingPattern('', 25),
+    move(560, 1600),
+
     setTicketCount(9),
     move(0, 0),
-    fontSize(3),
+    drawBox(50, 50, 5),
+    uploadTextLogo(useFont(3, 1, 1), 'Here is a text logo'),
     boxSize(33, 44),
+    uploadTTF(10, 'XASDPOISDfSDFOPISDFSDnlkNSDflSDLKJ'),
+    useTTF(10, 12),
+    'Here is something with a true-type font',
+    alignRight('Here is some text', 300),
     rotate(90),
-    code128Ladder('12345-6789-0', 9),
+    uploadImageLogo(1, 'ABCDEFGHIJKLMNOP', 'pcx'),
+    printLogo(1, 50, 60, 'pcx'),
+    code128('12345-6789-0', true, 9),
     qrCode('12345-6789-0'),
-    printCut()
+    print(false)
   ];
+
   console.info(cmds);
 }
